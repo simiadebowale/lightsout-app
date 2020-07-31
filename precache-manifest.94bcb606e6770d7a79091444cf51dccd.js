553016d8fc195d3fc8a518a9693dd2352303a4cd
Simi Adebowale <adebowalesimileoluwa@gmail.com>
2020-07-31 18:02:56 -0400

    Updates

---
+++ precache-manifest.94bcb606e6770d7a79091444cf51dccd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f67e909b7e39f65d8af",
+    "url": "/lightsout-app/static/css/main.2c894ff1.chunk.css"
+  },
+  {
+    "revision": "6f67e909b7e39f65d8af",
+    "url": "/lightsout-app/static/js/main.6f67e909.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/lightsout-app/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "c0a5a934b252b973121e",
+    "url": "/lightsout-app/static/js/runtime~main.c0a5a934.js"
+  },
+  {
+    "revision": "7de8bc16e86aedd48a0b08c4d08e43e3",
+    "url": "/lightsout-app/index.html"
+  }
+];
